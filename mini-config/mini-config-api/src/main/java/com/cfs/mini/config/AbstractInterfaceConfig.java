10f4be0edd1f0b7c5c44ffcd7da285074ff09127
cfad <cfad@cfaddeMacBook-Pro.local>
2018-08-19 12:48:36 +0800

    检验接口是否存在配置文件中方法

--- mini-config/mini-config-api/src/main/java/com/cfs/mini/config/AbstractInterfaceConfig.java
+++ mini-config/mini-config-api/src/main/java/com/cfs/mini/config/AbstractInterfaceConfig.java
@@ -4,7 +4,42 @@ import java.util.List;
 
 public  abstract class AbstractInterfaceConfig extends AbstractMethodConfig {
 
-    protected void checkInterfaceAndMethods(Class<?> interfaceClass, List<MethodConfig> methods) {
 
+
+    /**
+     * 检验接口是否存在配置文件中方法
+     * */
+    protected void checkInterfaceAndMethods(Class<?> interfaceClass, List<MethodConfig> methods) {
+         if(interfaceClass == null ){
+             throw new IllegalStateException("interface not allow null!");
+         }
+
+        //验证类是否是一个接口
+         if (!interfaceClass.isInterface()) {
+            throw new IllegalStateException("The interface class " + interfaceClass + " is not a interface!");
+        }
+
+        /**
+         * 检验接口是否存在当前方法,如果接口不存在这个方法,那么是无法进行暴露的
+         * 这种情况是防止配置是否显示配置了一个在接口中不存在的方法
+         * */
+        if(methods !=null && !methods.isEmpty()){
+             for(MethodConfig methodConfig:methods){
+                 String methodName = methodConfig.getName();
+                 if (methodName == null || methodName.length() == 0) {
+                     throw new IllegalStateException("<mini:method> name attribute is required! Please check: <dubbo:service interface=\"" + interfaceClass.getName() + "\" ... ><dubbo:method name=\"\" ... /></<dubbo:reference>");
+                 }
+                 boolean hasMethod = false;
+                 for (java.lang.reflect.Method method : interfaceClass.getMethods()) {
+                     if (method.getName().equals(methodName)) {
+                         hasMethod = true;
+                         break;
+                     }
+                 }
+                 if (!hasMethod) {
+                     throw new IllegalStateException("The interface " + interfaceClass.getName() + " not found method " + methodName);
+                 }
+             }
+        }
     }
 }
--- mini-config/mini-config-api/src/main/java/com/cfs/mini/config/MethodConfig.java
+++ mini-config/mini-config-api/src/main/java/com/cfs/mini/config/MethodConfig.java
@@ -1,4 +1,14 @@
 package com.cfs.mini.config;
 
 public class MethodConfig extends AbstractMethodConfig{
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
